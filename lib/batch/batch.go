feb10d86747b97b21682485ed4753032ad51a627
Turaj <mehdiev.t@gmail.com>
2022-07-10 17:16:44 +0600

    tt

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,22 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	goroutineLimiter := make(chan struct{}, pool)
+	var wg sync.WaitGroup
+	defer wg.Done()
+	var mu sync.Mutex
+	var i int64
+	for i = 0; i < n; i++ {
+		goroutineLimiter <- struct{}{}
+		wg.Add(1)
+		go func(next int64) {
+			nextUser := getOne(next)
+			mu.Lock()
+			res = append(res, nextUser)
+			mu.Unlock()
+			<-goroutineLimiter
+		}(i)
+	}
+	wg.Wait()
+	return res
 }
